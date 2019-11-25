1c14a3f0e075f6c1e494b5d3632863988f8203b0
danielpadkins <danielpadk@gmail.com>
2019-11-25 15:07:27 -0500

    layer sizes

--- model.py
+++ model.py
@@ -3,37 +3,27 @@ import tensorflow as tf
 import transformer_funcs as transformer
 
 class Transformer_Seq2Seq(tf.keras.Model):
-	def __init__(self, french_window_size, french_vocab_size, english_window_size, english_vocab_size):
+	def __init__(self, input_window_size, input_vocab_size, english_window_size):
 
-		######vvv DO NOT CHANGE vvv##################
 		super(Transformer_Seq2Seq, self).__init__()
 
-		self.french_vocab_size = french_vocab_size # The size of the french vocab
-		self.english_vocab_size = english_vocab_size # The size of the english vocab
+		self.review_vocab_size = input_vocab_size # The size of vocab from input reviews (preprocess.py)
+		self.score_size = 5 # The range of possible amazon customer reviews
 
-		self.french_window_size = french_window_size # The french window size
-		self.english_window_size = english_window_size # The english window size
-		######^^^ DO NOT CHANGE ^^^##################
+		self.review_window_size = review_window_size # The review window size
 
-
-		# TODO:
-		# 1) Define any hyperparameters
-		# 2) Define embeddings, encoder, decoder, and feed forward layers
-
-		# Define batch size and optimizer/learning rate
 		self.batch_size = 100
-		self.embedding_size = 100
+		self.embedding_size = 100 # CHANGE
 
 		self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
 
 		# Define english and french embedding layers:
-		self.FR_embedding = tf.Variable(tf.random.truncated_normal(shape=[self.french_vocab_size, self.embedding_size], stddev=0.1, dtype=tf.float32))
-		self.EN_embedding = tf.Variable(tf.random.truncated_normal(shape=[self.english_vocab_size, self.embedding_size], stddev=0.1, dtype=tf.float32))
+		self.review_embedding = tf.Variable(tf.random.truncated_normal(shape=[self.review_vocab_size, self.embedding_size], stddev=0.1, dtype=tf.float32))
+		self.score_embedding = tf.Variable(tf.random.truncated_normal(shape=[self.score_size, self.embedding_size], stddev=0.1, dtype=tf.float32))
 
-		# Create positional encoder layers
-		self.FR_pos_embedding = transformer.Position_Encoding_Layer(self.french_window_size, self.embedding_size)
-		self.EN_pos_embedding = transformer.Position_Encoding_Layer(self.english_window_size, self.embedding_size)
+		# Create positional encoder layer for reviews
+		self.review_pos_embedding = transformer.Position_Encoding_Layer(self.review_window_size, self.embedding_size)
 
 		# Define encoder and decoder layers:
 		self.encoder = transformer.Transformer_Block(self.embedding_size, False, False)
@@ -41,34 +31,25 @@ class Transformer_Seq2Seq(tf.keras.Model):
 
 		# Define dense layer(s)
 		self.dense1 = tf.keras.layers.Dense(units=self.embedding_size, activation=tf.nn.relu)
-		self.dense2 = tf.keras.layers.Dense(units=self.english_vocab_size, activation=tf.nn.softmax)
+		self.dense2 = tf.keras.layers.Dense(units=self.score_size, activation=tf.nn.softmax)
 
 
 	@tf.function
 	def call(self, encoder_input, decoder_input):
 		"""
-		:param encoder_input: batched ids corresponding to french sentences
-		:param decoder_input: batched ids corresponding to english sentences
-		:return prbs: The 3d probabilities as a tensor, [batch_size x window_size x english_vocab_size]
+		:param encoder_input: batched ids corresponding to reviews
+		:param decoder_input: batched scores
+		:return prbs: The 3d probabilities as a tensor, [batch_size x window_size x score_size]
 		"""
 
-		# TODO:
-		#1) Add the positional embeddings to french sentence embeddings
-		encode_embedding = tf.nn.embedding_lookup(self.FR_embedding, encoder_input)
-		decode_embedding = tf.nn.embedding_lookup(self.EN_embedding, decoder_input)
+		encode_embedding = tf.nn.embedding_lookup(self.review_embedding, encoder_input)
+		decode_embedding = tf.nn.embedding_lookup(self.score_embedding, decoder_input)
 
-		FR_pos_embedding = self.FR_pos_embedding(encode_embedding)
+		review_pos_embedding = self.review_pos_embedding(encode_embedding)
+		encoding = self.encoder(review_pos_embedding)
 
-		#2) Pass the french sentence embeddings to the encoder
-		encoding = self.encoder(FR_pos_embedding)
+		decoding = self.decoder(decode_embedding,encoding)
 
-		#3) Add positional embeddings to the english sentence embeddings
-		EN_pos_embedding = self.EN_pos_embedding(decode_embedding)
-
-		#4) Pass the english embeddings and output of your encoder, to the decoder
-		decoding = self.decoder(EN_pos_embedding,encoding)
-
-		#3) Apply dense layer(s) to the decoder out to generate probabilities
 		dense1 = self.dense1(decoding)
 		dense2 = self.dense2(dense1)
 
@@ -76,11 +57,7 @@ class Transformer_Seq2Seq(tf.keras.Model):
 
 	def accuracy_function(self, prbs, labels, mask):
 		"""
-		DO NOT CHANGE
-
-		Computes the batch accuracy
-
-		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x score_size]
 		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
 		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
 		:return: scalar tensor of accuracy of the batch between 0 and 1
@@ -95,7 +72,7 @@ class Transformer_Seq2Seq(tf.keras.Model):
 		"""
 		Calculates the model cross-entropy loss after one forward pass
 
-		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x score_size]
 		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
 		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
 		:return: the loss of the model as a tensor
--- run.py
+++ run.py
@@ -8,95 +8,25 @@ from rnn_model import RNN_Seq2Seq
 import sys
 
 ## IMPORTANT!!!!!! TAKE OUT ID2WORD!!!@!
-def train(model, train_french, train_english, eng_padding_index):
-	"""
-	Runs through one epoch - all training examples.
-
-	:param model: the initilized model to use for forward and backward pass
-	:param train_french: french train data (all data for training) of shape (num_sentences, 14)
-	:param train_english: english train data (all data for training) of shape (num_sentences, 15)
-	:param eng_padding_index: the padding index, the id of *PAD* token. This integer is used to mask padding labels.
-	:return: None
-	"""
-
-	# NOTE: For each training step, you should pass in the french sentences to be used by the encoder,
-	# and english sentences to be used by the decoder
-	# - The english sentences passed to the decoder have the last token in the window removed:
-	#	 [STOP CS147 is the best class. STOP *PAD*] --> [STOP CS147 is the best class. STOP]
-	#
-	# - When computing loss, the decoder labels should have the first word removed:
-	#	 [STOP CS147 is the best class. STOP] --> [CS147 is the best class. STOP]
-
-
-	for i in range(0, len(train_french)//model.batch_size):
-		sliced_fr = train_french[i * model.batch_size:model.batch_size*(i+1)]
-		sliced_en = train_english[i * model.batch_size :model.batch_size*(i+1)]
-
-
-		with tf.GradientTape() as tape:
-			predictions = model(sliced_fr, sliced_en[:,:-1])
-			decoded_symbols = tf.argmax(input=predictions, axis=2)
-
-
-			mask = tf.not_equal(sliced_en[:,1:], eng_padding_index)
-			loss = model.loss_function(predictions, sliced_en[:,1:], mask)
-			acc = model.accuracy_function(predictions, sliced_en[:, 1:], mask)
-			if i % 10 == 0:
-
-				print("Step", i, ":", loss.numpy(), ",", acc.numpy(), "%")
-
-		gradients = tape.gradient(loss, model.trainable_variables)
-		model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+def train(model, train_reviews, train_scores):
 	pass
 
-def test(model, test_french, test_english, eng_padding_index):
-	"""
-	Runs through one epoch - all testing examples.
-
-	:param model: the initilized model to use for forward and backward pass
-	:param test_french: french test data (all data for testing) of shape (num_sentences, 14)
-	:param test_english: english test data (all data for testing) of shape (num_sentences, 15)
-	:param eng_padding_index: the padding index, the id of *PAD* token. This integer is used to mask padding labels.
-	:returns: perplexity of the test set, per symbol accuracy on test set
-	"""
-
-	loss = 0
-	acc = 0
-	for i in range(0, len(test_french)//model.batch_size):
-		sliced_fr = test_french[i * model.batch_size:model.batch_size*(i+1)]
-		sliced_en = test_english[i * model.batch_size :model.batch_size*(i+1)]
-
-
-
-		predictions = model(sliced_fr, sliced_en[:,:-1])
-		mask = tf.not_equal(sliced_en[:,1:], eng_padding_index)
-		loss += model.loss_function(predictions, sliced_en[:,1:], mask)
-		acc += model.accuracy_function(predictions, sliced_en[:, 1:], mask)
-	# Note: Follow the same procedure as in train() to construct batches of data!
-	return (tf.exp(loss/(len(test_french)//model.batch_size))).numpy(),(acc/(len(test_french)//model.batch_size)).numpy()
+def test(model, test_reviews, test_scores):
+	pass
 
 def main():
-	if len(sys.argv) != 2 or sys.argv[1] not in {"RNN","TRANSFORMER"}:
-			print("USAGE: python assignment.py <Model Type>")
-			print("<Model Type>: [RNN/TRANSFORMER]")
-			exit()
-
 	print("Running preprocessing...")
-	train_english,test_english, train_french,test_french, english_vocab,french_vocab,eng_padding_index = get_data('data/fls.txt','data/els.txt','data/flt.txt','data/elt.txt')
+	train_scores,test_scores, train_reviews,test_reviews, scores_vocab,reviews_vocab = get_data()
 	print("Preprocessing complete.")
 
-	model_args = (FRENCH_WINDOW_SIZE,len(french_vocab),ENGLISH_WINDOW_SIZE, len(english_vocab))
-	if sys.argv[1] == "RNN":
-		model = RNN_Seq2Seq(*model_args)
-	elif sys.argv[1] == "TRANSFORMER":
-		model = Transformer_Seq2Seq(*model_args)
+	model = Transformer_Seq2Seq(*model_args)
 
-	id2word = {v: k for k, v in english_vocab.items()}
+	id2word = {v: k for k, v in scores_vocab.items()}
 
-	# TODO:
-	# Train and Test Model for 1 epoch.
-	train(model, train_french, train_english, eng_padding_index)
-	loss, acc = test(model, test_french, test_english, eng_padding_index)
+	print("Training model.")
+	train(model, train_reviews, train_scores, eng_padding_index)
+	print("Training complete.")
+	loss, acc = test(model, test_reviews, test_scores, eng_padding_index)
 	print(loss, acc)
 
 if __name__ == '__main__':
