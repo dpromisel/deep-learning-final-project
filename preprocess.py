61393d1ad56b5c99f0a77a217c806cfcd2be9b8b
David Promisel <davidpromisel@Davids-MacBook-Pro.local>
2019-11-26 14:28:34 -0500

    Basic Reading Data

---
+++ preprocess.py
@@ -0,0 +1,46 @@
+import numpy as np
+import tensorflow as tf
+import numpy as np
+import csv
+
+##########DO NOT CHANGE#####################
+REVIEW_FILE = "./data/amazon_camera_reviews.tsv"
+SAMPLE_FILE = "./data/sample_us.tsv"
+##########DO NOT CHANGE#####################
+
+def read_data(file_name):
+	"""
+	DO NOT CHANGE
+
+  Load text data from file
+
+	:param file_name:  string, name of data file
+	:return: list of sentences, each a list of words split on whitespace
+  """
+	reviews = []
+	with open(file_name, 'rt') as data_file:
+		reader = csv.DictReader(data_file, dialect='excel-tab')
+		for row in reader:
+			reviews.append(row)
+
+	return reviews
+
+
+def get_data():
+	"""
+	"""
+	
+	#1) Read Review Data for training and testing (see read_data)
+	reviews = read_data(SAMPLE_FILE)
+
+	inputs = []
+	labels = []
+
+	for review in reviews:
+		labels.append(review["star_rating"])
+		inputs.append(review["review_headline"] + " " + review["review_body"])
+	
+	print(inputs, labels)
+
+	return inputs, labels, [], [], [], []
+	
--- run.py
+++ run.py
@@ -3,11 +3,10 @@ import numpy as np
 import tensorflow as tf
 import numpy as np
 from preprocess import *
-from transformer_model import Transformer_Seq2Seq
-from rnn_model import RNN_Seq2Seq
+# from transformer_model import Transformer_Seq2Seq
+# from rnn_model import RNN_Seq2Seq
 import sys
 
-## IMPORTANT!!!!!! TAKE OUT ID2WORD!!!@!
 def train(model, train_reviews, train_scores):
 	pass
 
@@ -16,18 +15,18 @@ def test(model, test_reviews, test_scores):
 
 def main():
 	print("Running preprocessing...")
-	train_scores,test_scores, train_reviews,test_reviews, scores_vocab,reviews_vocab = get_data()
+	train_scores,test_scores, train_reviews,test_reviews, scores_vocab, reviews_vocab = get_data()
 	print("Preprocessing complete.")
 
-	model = Transformer_Seq2Seq(*model_args)
+	# model = Transformer_Seq2Seq(*model_args)
 
-	id2word = {v: k for k, v in scores_vocab.items()}
+	# id2word = {v: k for k, v in scores_vocab.items()}
 
-	print("Training model.")
-	train(model, train_reviews, train_scores, eng_padding_index)
-	print("Training complete.")
-	loss, acc = test(model, test_reviews, test_scores, eng_padding_index)
-	print(loss, acc)
+	# print("Training model.")
+	# train(model, train_reviews, train_scores, eng_padding_index)
+	# print("Training complete.")
+	# loss, acc = test(model, test_reviews, test_scores, eng_padding_index)
+	# print(loss, acc)
 
 if __name__ == '__main__':
    main()
